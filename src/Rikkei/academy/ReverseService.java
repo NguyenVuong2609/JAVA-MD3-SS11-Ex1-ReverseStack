66952d794f106163fbe526fa5b5dd13b9fe695c2
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-17 13:51:34 +0700

    1703

--- src/Rikkei/academy/ReverseService.java
+++ src/Rikkei/academy/ReverseService.java
@@ -3,22 +3,26 @@ package Rikkei.academy;
 import java.util.Arrays;
 
 public class ReverseService {
-    public static void main(String[] args) {
-        MyStack<Integer> test = new MyStack<>(5);
-        test.push(1);
-        test.push(2);
-        test.push(3);
-        test.push(4);
-        test.push(5);
-        System.out.println("Mảng ban đầu -->" + test);
-        System.out.println("Sau khi reverse -->" + Arrays.toString(test.reverseStack()));
-        MyStack<String> testString = new MyStack<>(5);
-        testString.push("Yen");
-        testString.push("Chuan");
-        testString.push("Vuong");
-        testString.push("Van");
-        testString.push("Thu");
-        System.out.println("Mảng ban đầu -->" + testString);
-        System.out.println("Sau khi reverse -->" + Arrays.toString(testString.reverseStack()));
+    public static void main(String[] args) throws Exception {
+       int[] arr = {1,2,3,4,5};
+        System.out.println("Mảng ban đầu là --> " + Arrays.toString(arr));
+        MyStack<Integer> reverseArr = new MyStack<>(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            reverseArr.push(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = reverseArr.pop();
+        }
+        System.out.println("Sau khi reverse --> " + Arrays.toString(arr));
+        String[] stringArr = {"Chuan","Yen","Vuong","Van","Thu"};
+        System.out.println("Mảng ban đầu là --> " + Arrays.toString(stringArr));
+        MyStack<String> reverseStringArr = new MyStack<>(stringArr.length);
+        for (int i = 0; i < stringArr.length; i++) {
+            reverseStringArr.push(stringArr[i]);
+        }
+        for (int i = 0; i < stringArr.length; i++) {
+            stringArr[i] = reverseStringArr.pop();
+        }
+        System.out.println("Sau khi reverse --> " + Arrays.toString(stringArr));
     }
 }
